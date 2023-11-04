5504325bc66644e2aee2ac33aaadb921b4d6bf40
lexsorokin <lex.sorokin.dev@gmail.com>
2023-11-04 15:25:19 +0300

    hash table collision problem

--- hash_table.py
+++ hash_table.py
@@ -3,10 +3,13 @@
 # look up by key = O(1)
 # Insertion and deletion = O(1)
 
+# Collision handling - using linear probing
+# Linear probing - basically searching for the next closest slot in the hash table 
+
 class HashTable: 
     def __init__(self):
         self.MAX = 100
-        self.arr = [None for i in range(self.MAX)] # init 100 elems in array
+        self.arr = [[] for _ in range(self.MAX)] # init 100 elems in array
 
     def get_hash(self, key): 
         h = 0 
@@ -16,27 +19,37 @@ class HashTable:
     
     def __setitem__(self, key, value): # * <- a python operator, that allows to set item like this hd[key] = value 
         h = self.get_hash(key) # get an array index of the key 
-        self.arr[h] = value # assign a value to received index of the array
+        found = False
+        for idx, element in enumerate(self.arr[h]): 
+            if len(element) == 2 and element[0] == key: 
+                self.arr[h][idx] = (key, value)
+                found = True
+                break
+        if not found: 
+            self.arr[h].append((key, value))
 
     def __getitem__(self, key): # * <- a python operator, that allows to get item like this hd[key]
         h = self.get_hash(key) # get an array index of the key
-        return self.arr[h] # get a value from array by index
-    
+        for element in self.arr[h]: 
+            if element[0] == key: 
+                return element[1]
+
     def __delitem__(self, key):
         h = self.get_hash(key) 
-        self.arr[h] = None 
+        for index, element in enumerate(self.arr[h]): 
+            if element[0] == key: 
+                del self.arr[h][index]
     
 ht = HashTable()
-key = "march 6"
-hash = ht.get_hash(key)
-print(hash)
-# ht.add(key=key, value=1234)
-ht[key] = 2131521
-ht["december 17"] = 25424
-ht["march 23"] = 4344
-print(ht.arr)
-# val = ht.get(key)
-print(ht[key])
 
-del ht[key]
+ht["march 6"]=213
+ht["march 5"]=2136
+ht["march 17"] = 214
+ht["march 6"] = 2145
+ht["march 8"] = 123
+ht["march 9"] = 1456
+ 
+print(ht["march 17"])
+del ht["march 17"]
 print(ht.arr)
+
